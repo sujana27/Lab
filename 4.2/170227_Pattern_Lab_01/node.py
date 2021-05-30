ed96a84ba12384f809a0a292014762820c0edfb9
sujana27 <sujanasaha27@gmail.com>
2021-05-30 21:04:15 +0600

    4.2/170227_Pattern_Lab_01

--- 4.2/170227_Pattern_Lab_01/node.py
+++
@@ -1,9 +0,0 @@
-class Node:
-
-    def __init__(self, attribute: str, previous_attribute_value: str, decision: str, children: list):
-        self.attribute = attribute
-        self.decision = decision
-        self.children = []
-        self.previous_attribute_value = previous_attribute_value
-
-    
